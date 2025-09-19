a504f90cdc9b8157ea46a7221978e35c5aaf0a96
Clocky97 <milagrosruizdiazz150@gmail.com>
2025-09-19 15:06:35 -0300

    modelo para users creado correctamente

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: { type: String, required: true, unique: true, minLength: 3, maxLength: 20 },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        role: { type: String, enum: ['user', 'admin'], default: 'user' },
+        profile: {
+            firstName: { type: String, minLength: 2 ,maxLength: 50},
+            lastName: { type: String, minLength: 2, maxLength: 50 },
+            biography: { type: String, maxLength: 500, required: false },
+            avatarUrl: { type: String, required: false, isUrl: true },
+            bithDate: { type: Date, required: false}
+        }
+    },
+    { timestamps: true
+    }
+);
+
+export const UserModel = model("User", userSchema);
