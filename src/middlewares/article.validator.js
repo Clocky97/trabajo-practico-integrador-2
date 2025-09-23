4ada07debec1f7efcac4a4f4b5377cd931636dd8
Clocky97 <milagrosruizdiazz150@gmail.com>
2025-09-23 16:41:25 -0300

    guardado de emergencia

--- src/middlewares/article.validator.js
+++ src/middlewares/article.validator.js
@@ -3,10 +3,10 @@ import { body } from "express-validator";
 export const validateArticle = [
     body('title')
         .notEmpty().withMessage('El titulo es obligatorio')
-        .isLength({ min: 5, max: 150 }).withMessage('El titulo debe tener entre 5 y 150 caracteres'),
+        .isLength({ min: 3, max: 300 }).withMessage('El titulo debe tener entre 3 y 300 caracteres'),
     body('content')
         .notEmpty().withMessage('El contenido no pude estar vacio')
-        .isLength({ min: 20 }).withMessage('El contenido debe tener al menos 20 caracteres'),
+        .isLength({ min: 50 }).withMessage('El contenido debe tener al menos 50 caracteres'),
     body('exerpt')
         .optional()
         .isLength({ max: 500 }).withMessage('El extracto no debe superar los 500 caracteres'),
--- src/middlewares/comment.validator.js
+++ src/middlewares/comment.validator.js
@@ -3,5 +3,5 @@ import { body } from "express-validator";
 export const validateComment = [
     body('content')
         .notEmpty().withMessage('Debe comentar algo')
-        .isLength({ min: 1, max: 1000 }).withMessage('El comentario debe tener entre 1 y 1000 caracteres'),
+        .isLength({ min: 1, max: 500 }).withMessage('El comentario debe tener entre 1 y 500 caracteres'),
 ]
